2d6cdf704994fc86d642b65103daac1d9ab6dbe7
Alfred Beckman <alfred@salmiakmedia.se>
2019-06-24 21:07:38 +0200

    Added purecss

--- src/header.php
+++ src/header.php
@@ -11,16 +11,22 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
 <?php wp_head(); ?>
 </head>
 
 <body>
-
-<h1><?php bloginfo('name'); ?></h1>
-<h2><?php bloginfo('description'); ?></h2>
+<div class="pure-g">
+  <div class="pure-u-1-3 l-box">
+    <h3 class="logo"><?php bloginfo('name'); ?></h3>
+    <h1 class="logo">Prorenata</h1>
+  </div>
+  <div class="pure-u-2-3 l-box">
+    <h2><?php bloginfo('description'); ?></h2>
+  </div>
+</div>
 
 This is the header template (header.php):<br />
 
