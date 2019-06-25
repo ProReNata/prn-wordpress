575ad2647e877e59c5aab425b9679faa627f9253
Alfred Beckman <alfred@salmiakmedia.se>
2019-06-25 23:23:54 +0200

    Added more sections to front page

--- src/front-page.php
+++ src/front-page.php
@@ -14,12 +14,11 @@ get_header(); ?>
 	</div>
 </div>
 
-<div class="quote">
+<div class="quote bg-a500">
 	<div class="pure-g"><div class="pure-u-1-1 l-box">
 		<blockquote>”I mitt arbete som läkare har jag upplevt ett stort behov av ett enkelt, lättöverskådligt dokumentationssystem. Därför har vi skapat Prorenata Journal.”</blockquote>
 		<div class="quotee">Jens Alm, Grundare och VD <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/jens.jpg" class="quotee-img" /> Läs hela brevet från Jens -></div>
-	</div>
-</div>
+	</div></div>
 </div>
 
 
@@ -70,5 +69,38 @@ get_header(); ?>
 		</div>
 	</div>
 </div>
+
+<div class="bg-s300">
+	<div class="pure-g">
+		<div class="pure-u-1-2 l-box">
+			<span class="strong">Moduler i fokus</span>
+		</div>
+		<div class="pure-u-1-2 l-box text-right">
+			Se alla moduler ->
+		</div>
+	</div>
+</div>
+
+<div class="quote bg-s400">
+	<div class="pure-g"><div class="pure-u-1-1 l-box">
+		<blockquote>”Övergången från tidigare journalsystem har känts trygg tack vare bra support och mycket stöd från personal på Prorenata.”</blockquote>
+		<div class="quotee">Anna Jansson, skolsköterska  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/annajansson.png" class="quotee-img" /> Internationella Engelska Skolan</div>
+	</div></div>
+</div>
+
+<div class="section bg-a500 text-center">
+	<div class="pure-g"><div class="pure-u">
+		<p><a href="#" class="btn btn-primary">Boka en demo</a></p>
+		<p>
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/brita.jpg" class="img-circle img-avatar" />
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/sophia.jpg" class="img-circle img-avatar" />
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/martina.jpg" class="img-circle img-avatar" />
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/mia.jpg" class="img-circle img-avatar" />
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/sanna.jpg" class="img-circle img-avatar" />
+		</p>
+		<p>…eller slå oss en signal på <strong>010-330 06 70</strong> - vårt kundteam står redo.</p>
+	</div></div>
+</div>
+
 
 <?php get_footer(); ?>
