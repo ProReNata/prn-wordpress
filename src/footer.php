7326e56ec05e0458dfbcf7a229f977029f5ca37a
Alfred Beckman <alfred@salmiakmedia.se>
2019-06-26 23:55:23 +0200

    Added a footer

--- src/footer.php
+++ src/footer.php
@@ -2,14 +2,35 @@
 /**
  * The template for displaying the footer.
  *
- * @package WordPress
- * @subpackage themename
  */
 ?>
 
-<hr />
+<footer>
+  <div class="pure-g">
+    <div class="pure-u-1-1 pure-u-lg-7-24 l-box">
+      <h3 class="logo logo-white-text"><?php bloginfo('name'); ?></h3>
+    </div>
+    <div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 pure-u-lg-5-24 l-box">
+      Menu 1
+    </div>
+    <div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 pure-u-lg-5-24 l-box">
+      Menu 2
+    </div>
+    <div class="pure-u-1-1 pure-u-sm-1-3 pure-u-md-2-4 pure-u-lg-7-24 l-box">
+      <p>010-330 06 70<br />
+        prorenata@prorenata.se</p>
+      <p>Skeppsbron 30 <br />
+        111 30 Stockholm</p>
+      <p>Orgnr. 556827-5555</p>
+    </div>
+  </div>
+  <div class="pure-g">
+    <div class="pure-u-1-1 l-box text-small">
+      Copyright &copy; 2019
+    </div>
+  </div>
+</footer>
 
-This is the footer template (footer.php)! Yes!!
 
 <?php wp_footer(); ?>
 
