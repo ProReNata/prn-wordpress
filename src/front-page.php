99a1749dc9a901cd891eec1d77dbb0e09678bdf6
Alfred Beckman <alfred@salmiakmedia.se>
2019-06-27 23:28:13 +0200

    Improved sales team presentation on front page

--- src/front-page.php
+++ src/front-page.php
@@ -101,17 +101,31 @@ get_header(); ?>
 </div>
 
 <div class="section bg-a500 text-center">
-	<div class="pure-g"><div class="pure-u">
-		<p><a href="#" class="btn btn-primary">Boka en demo</a></p>
-		<p>
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/brita.jpg" class="img-circle img-avatar" />
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/sophia.jpg" class="img-circle img-avatar" />
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/martina.jpg" class="img-circle img-avatar" />
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/mia.jpg" class="img-circle img-avatar" />
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/sanna.jpg" class="img-circle img-avatar" />
-		</p>
-		<p>…eller slå oss en signal på <strong>010-330 06 70</strong> - vårt kundteam står redo.</p>
-	</div></div>
+	<div class="pure-g">
+		<div class="pure-u-1-1 l-box">
+			<a href="#" class="btn btn-primary">Boka en demo</a>
+		</div>
+		<div class="pure-u-1-1 pure-u-sm-3-4 pure-u-md-1-2"><div class="pure-g">
+			<div class="pure-u-1-5">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/brita.jpg" class="img-circle img-avatar" />
+			</div>
+			<div class="pure-u-1-5">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/sophia.jpg" class="img-circle img-avatar" />
+			</div>
+			<div class="pure-u-1-5">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/martina.jpg" class="img-circle img-avatar" />
+			</div>
+			<div class="pure-u-1-5">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/mia.jpg" class="img-circle img-avatar" />
+			</div>
+			<div class="pure-u-1-5">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/sanna.jpg" class="img-circle img-avatar" />
+			</div>
+		</div></div>
+		<div class="pure-u-1-1 l-box">
+			…eller slå oss en signal på <strong>010-330 06 70</strong> - vårt kundteam står redo.
+		</div>
+	</div>
 </div>
 
 <div class="section text-center features">
