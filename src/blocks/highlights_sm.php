05486c0770ddb3c66852a79006e166cd13dc3bdc
Alfred Beckman <alfred@salmiakmedia.se>
2019-06-30 23:02:44 +0200

    Connected data into Small highlights block

--- src/blocks/highlights_sm.php
+++ src/blocks/highlights_sm.php
@@ -1,54 +1,34 @@
 <div class="section text-center features">
 	<div class="pure-g">
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-search fa-2x"></i></p>
-			<p>Supersnabb sök</p>
-		</div>
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-certificate fa-2x"></i></p>
-			<p>CE-märkt</p>
-		</div>
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-key fa-2x"></i></p>
-			<p>Tvåfaktorsinloggning</p>
-		</div>
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-tablet fa-2x"></i></p>
-			<p>Fullt stöd för iPad</p>
-		</div>
 
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-chart-pie fa-2x"></i></p>
-			<p>Kraftfulla rapporter</p>
-		</div>
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-save fa-2x"></i></p>
-			<p>Automatisk säkerhetskopiering</p>
-		</div>
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-globe-europe fa-2x"></i></p>
-			<p>Tillgängligt överallt</p>
-		</div>
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-headset fa-2x"></i></p>
-			<p>Superb support</p>
-		</div>
+		<?php
 
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-users fa-2x"></i></p>
-			<p>Sekretess mellan yrkeskategorier</p>
-		</div>
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-reply fa-2x"></i></p>
-			<p>Uppföljning av anteckningar</p>
-		</div>
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-keyboard fa-2x"></i></p>
-			<p>Enkel inmatning</p>
-		</div>
-		<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
-			<p><i class="fal fa-book fa-2x"></i></p>
-			<p>Fullständig historik</p>
-		</div>
+// check if the repeater field has rows of data
+if( have_rows('highlights') ):
+
+ 	// loop through the rows of data
+    while ( have_rows('highlights') ) : the_row();
+
+        // display a sub field value
+        $icon = get_sub_field('icon');
+				$label = get_sub_field('label');
+				?>
+
+				<div class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 l-box">
+					<p><i class="<?php echo $icon; ?> fa-2x"></i></p>
+					<p><?php echo $label; ?></p>
+				</div>
+
+				<?php
+
+    endwhile;
+
+else :
+
+    echo "Du måste lägga till logotyper";
+
+endif;
+
+?>
 	</div>
 </div>
