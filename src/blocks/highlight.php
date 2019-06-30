bed552178825661d10bcef9669f1760c64cd4193
Alfred Beckman <alfred@salmiakmedia.se>
2019-06-30 22:46:05 +0200

    Hooked in data in the highlight block module

--- src/blocks/highlight.php
+++ src/blocks/highlight.php
@@ -1,15 +1,32 @@
+<?php
+
+// Load values and assing defaults.
+$suptitle = get_field('suptitle') ?: 'Suptitle goes here';
+$title = get_field('title') ?: 'Title goes here';
+$description = get_field('description') ?: 'Description goes here';
+$image = get_field('image') ?: null;
+$dir = get_field('img_side') ?: 'left';
+
+ ?>
+
 <div class="section">
 	<div class="pure-g">
-		<div class="pure-u-1-1 hide-md text-center">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/ill-support.svg" alt="" class="pure-img">
+		<?php if ($dir == 'left') {
+			echo '<div class="pure-u-1-1 pure-u-md-7-12 text-center">';
+		} else {
+			echo '<div class="pure-u-1-1 hide-md text-center">';
+		} ?>
+			<img src="<?php echo $image['url'] ?>" alt="" class="pure-img">
 		</div>
 		<div class="pure-u-1-1 pure-u-md-5-12 l-box">
-			<h5>Fri support</h5>
-			<h4>Snabb & personlig support</h4>
-			<p>Vår support finns till för våra användare – därför är den alltid inkluderad i avtalet. Vi hjälper er varje vardag mellan kl&nbsp;08&nbsp;-&nbsp;17. Vi är snabba och effektiva, därför ligger vi ofta nära 100% kundnöjdhet. Det ska vara tryggt och enkelt att kontakta oss!</p>
+			<h5><?php echo $suptitle; ?></h5>
+			<h4><?php echo $title; ?></h4>
+			<?php echo $description; ?>
 		</div>
-		<div class="pure-u-md-7-12 show-md text-center">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/ill-support.svg" alt="" class="pure-img">
-		</div>
+		<?php if ($dir == 'right') { ?>
+			<div class="pure-u-md-7-12 show-md text-center">
+				<img src="<?php echo $image['url'] ?>" alt="" class="pure-img">
+			</div>
+		<?php } ?>
 	</div>
 </div>
