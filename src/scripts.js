a944dd7052ca1c63e67d13025f0476ea051631f3
Alfred Beckman <alfred@salmiakmedia.se>
2019-07-12 17:48:10 +0200

    Fixed styling of slider

--- src/scripts.js
+++ src/scripts.js
@@ -5,5 +5,7 @@ $('.module-slides').slick({
   infinite: true,
   speed: 300,
   centerMode: true,
-  variableWidth: true
+  variableWidth: true,
+  nextArrow: '<div class="slick-arrow-next"><i class="fal fa-arrow-right fa-fw"></i></div>',
+  prevArrow: '<div class="slick-arrow-prev"><i class="fal fa-arrow-left fa-fw"></i></div>'
 })
