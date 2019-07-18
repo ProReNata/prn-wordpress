4ffa53fd9b704b53c5380eac2df91c6d83f5dd59
Alfred Beckman <alfred@salmiakmedia.se>
2019-07-18 11:28:06 +0200

    Navigation on small screens

--- src/header.php
+++ src/header.php
@@ -19,14 +19,52 @@
 <body <?php body_class() ?>>
 <nav>
   <div class="pure-g">
-    <div class="pure-u-3-4">
+    <div class="pure-u-3-4 prn-show-md">
       <a class="logo" href="/"><?php bloginfo('name'); ?></a>
       <?php wp_nav_menu( array( 'theme_location' => 'header-menu-left' ) ); ?>
     </div>
-    <div class="pure-u-1-4 text-right">
+    <div class="pure-u-1-4 prn-show-md text-right">
       <?php wp_nav_menu( array( 'theme_location' => 'header-menu-right' ) ); ?>
+    </div>
+
+    <div class="prn-hide-md pure-u-1-2">
+      <a class="logo" href="/"><?php bloginfo('name'); ?></a>
+    </div>
+    <div class="prn-hide-md pure-u-1-2 text-right">
+      <ul>
+        <li><a href="javascript:showMenu()"><i class="fas fa-bars fa-lg"></i></a></li>
+      </ul>
     </div>
   </div>
 </nav>
+
+<script>
+showMenu = function() {
+  document.querySelector('#menu').classList.add('isActive');
+  document.querySelector('body').classList.add('drawer-open');
+  return false;
+}
+hideMenu = function() {
+  document.querySelector('#menu').classList.remove('isActive');
+  document.querySelector('body').classList.remove('drawer-open');
+  return false;
+}
+</script>
+
+<div class="drawer" id="menu">
+  <div class="close" onClick="hideMenu()">
+    <i class="fal fa-times fa-fw"></i>
+  </div>
+  <div class="drawer-content">
+    <div class="pure-g">
+      <div class="pure-u-1-1 pure-u-sm-3-5 pure-u-md-1-2 l-box pure-center">
+        <ul><li><a href="/">Hem</a></li></ul>
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu-left' ) ); ?>
+        <ul><li>&mdash;</li></ul>
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu-right' ) ); ?>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php // wp_nav_menu(); ?>
