4e03592f20ed7bb332c92cc6ca697c8454dd7bd4
Alfred Beckman <alfred@salmiakmedia.se>
2019-11-11 10:34:51 +0100

    Added a 404-page

---
+++ src/404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The Template for displaying a 404-page.
+ *
+ * @package WordPress
+ * @subpackage themename
+ */
+ get_header(); ?>
+
+ 	<div class="page-header">
+ 	  <div class="page-header-content">
+ 			<h1 style="font-size: 35vw; line-height: 35vw;">404</h1>
+ 		</div>
+ 	</div>
+
+ 	<article class="text-center">
+    <h2>Sidan kunde inte hittas</h2>
+ 		<p class="leading">Kontrollera URL:en och<br/><a href="mailto:info@prorenata.se">kontakta oss</a> om felet kvarstår.</p>
+ 	</article>
+
+ <?php get_footer(); ?>
