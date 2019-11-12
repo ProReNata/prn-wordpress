df19399aa62585f36db60a14410ba2b3f2a3f769
Alfred Beckman <alfred@salmiakmedia.se>
2019-11-12 11:57:51 +0100

    Added favicon

--- src/header.php
+++ src/header.php
@@ -11,6 +11,17 @@
 <meta charset="<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri()?>/assets/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri()?>/assets/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri()?>/assets/icons/favicon-16x16.png">
+<link rel="manifest" href="<?php echo get_template_directory_uri()?>/assets/icons/site.webmanifest">
+<link rel="mask-icon" href="<?php echo get_template_directory_uri()?>/assets/icons/safari-pinned-tab.svg" color="#f5a800">
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri()?>/assets/icons/favicon.ico">
+<meta name="msapplication-TileColor" content="#00303C">
+<meta name="msapplication-config" content="<?php echo get_template_directory_uri()?>/assets/icons/browserconfig.xml">
+<meta name="theme-color" content="#E5EBEE">
+
 <script src="https://kit.fontawesome.com/89c5779e5f.js"></script>
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
 <?php wp_head(); ?>
